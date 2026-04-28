a9493e178be80bbe84e49601709409cc2fd46c54
Rafaela Bourdette <rafaelabourdette@gmail.com>
2026-04-28 18:03:50 -0300

    :tada: First commit

---
+++ to-do-list/src/main/java/io/github/to_do_list/model/Task.java
@@ -0,0 +1,33 @@
+package io.github.to_do_list.model;
+
+import jakarta.annotation.Nullable;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name="task")
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    private String description;
+
+    @Column(nullable = false)
+    private boolean isCompleted;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+}
---
+++ to-do-list/src/main/java/io/github/to_do_list/model/User.java
@@ -0,0 +1,28 @@
+package io.github.to_do_list.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @OneToMany(mappedBy = "user")
+    private List<Task> allTasks;
+    private int countTasks;
+}
